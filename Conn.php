9e3ce6b86815e362b6f3c8c8332424491993563f
Roboitey <Islerih25@gmail.com>
2022-06-11 13:54:50 -0400

    connection addition

---
+++ Conn.php
@@ -0,0 +1,10 @@
+<?php
+    $servername = "localhost";
+    $username = "hscc";
+    $connection = mysqli_connect($servername, $username);
+    if(!connection)
+    {
+        die('could not connect!');
+    }
+?>
+
