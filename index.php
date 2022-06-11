b104c76af62fa36edc01a4ad5a890cd0cd21d4bf
Kenya Herring <51855473+hsccdc@users.noreply.github.com>
2022-06-11 09:53:32 -0400

    Set up Bootstrap Script to help team include bootstrap in webpages

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <head>
+        <title>One Piece</title>
+        <?php
+            require('bootstrap_script.html');
+        ?>
+    </head>
+    <body>
+        <!--Your code starts here-->
+    </body>
+</html>
