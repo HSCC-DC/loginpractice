e7a9e38baf1ce16e29ca8735cb2b7c2976b582d1
SaintLupe <aegmartinezgraham@gmail.com>
2022-06-11 10:56:04 -0400

    More changes

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
     <body>
         <!--Your code starts here-->
         <h1>One Piece - Movie Ticket Purchasing</h1>
-        <img src="one-piece-red.png" alt="One Piece Red">
+        <img src="one-piece-red.png" alt="One Piece Red" width="500px" height="250px">
     </body>
 </html>
