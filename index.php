59d021d35341fdb2fe94bdc4648384ce0c600a75
SaintLupe <aegmartinezgraham@gmail.com>
2022-06-11 13:56:51 -0400

    Changed the dropdown...

--- index.php
+++ index.php
@@ -30,17 +30,25 @@
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
-            <div class="mb-3">
-                <label for="numberOfAttendees" class="form-label">Number of Tickets</label>
-                <select id="numberOfAttendees" class="form-select">
-                <option selected>Choose...</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>5 or more</option>
-                </select>
+            <div class="dropdown">
+                <label for="movieShowtime" class="form-label">Movie Showtime</label>
+                <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    Choose...
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item" href="#">12:00</a></li>
+                    <li><a class="dropdown-item" href="#">13:00</a></li>
+                    <li><a class="dropdown-item" href="#">13:30</a></li>
+                    <li><a class="dropdown-item" href="#">15:30</a></li>
+                    <li><a class="dropdown-item" href="#">16:15</a></li>
+                    <li><a class="dropdown-item" href="#">17:00</a></li>
+                    <li><a class="dropdown-item" href="#">19:00</a></li>
+                    <li><a class="dropdown-item" href="#">19:45</a></li>
+                    <li><a class="dropdown-item" href="#">20:15</a></li>
+                    <li><a class="dropdown-item" href="#">22:15</a></li>
+                    <li><a class="dropdown-item" href="#">23:00</a></li>
+                    <li><a class="dropdown-item" href="#">23:30</a></li>
+                </ul>
             </div>
             <div class="col-md-2">
             <label for="dateOfBirth" class="form-label">Date of Birth</label>
