b2c42cbb9f66054448e5e6ca20db8c2641925cf8
SaintLupe <aegmartinezgraham@gmail.com>
2022-06-25 09:54:36 -0400

    Forgot to commit...hope it works...fingers crossed...

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
         <title>One Piece</title>
         <?php
             require('bootstrap_script.html');
-            require('insert_user.php');
         ?>
         <style>
             body {
@@ -16,7 +15,35 @@
     </head>
     <body >
         <!--Your code starts here-->
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                    <a class="navbar-brand" href="#">One Piece: Red - Movie Registration</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="view_users.php">View Users</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">                                
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    </div>
+            </div>
+        </nav><br>
         <h2 style="color:white; text-align: center;">One Piece - Movie Ticket Purchasing</h2><br>
+        <?php
+            require('insert_user.php');
+        ?>
             <div class="d-flex justify-content-center align-items-center">
                 <form>
                     <div class="col-md-8 ">
@@ -59,6 +86,7 @@
                         <label class="form-check-label" for="exampleCheck1">I am not a robot. I swear!</label>
                     </div><br>
                     <button type="submit" class="btn btn-success" name="submit">Submit Purchase(s)</button>
+
              </form>
         </div>
     </body>
